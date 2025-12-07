a9a39ea1a83cbd2d01ae8b50b201b33079b0506c
dinesh <dinesh.bisht@gmail.com>
2025-12-07 13:52:54 +0200

    update main repo

--- numpydemo/7-where_demo.py
+++ numpydemo/7-where_demo.py
@@ -2,7 +2,7 @@ import numpy as np
 
 array_1d = np.array([1,2,3,4,5,6,7,9,10])
 
-print("even array using where", array_1d[np.where(array_1d % 2 ==0)]) # this is similar to fancy indexing
+print("even array using where", array_1d[np.where(array_1d % 2 == 0)]) # this is similar to fancy indexing
 print("calculated array using where", np.where(array_1d % 2 == 0, array_1d*2, array_1d))
 
 ## fancy indexing
