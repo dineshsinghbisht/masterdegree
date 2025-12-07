90e09d41c5475f013d06c1c62588933dc18298ca
dinesh <dinesh.bisht@gmail.com>
2025-12-07 20:50:56 +0200

    update main repo

--- numpydemo/6-filtering_demo.py
+++ numpydemo/6-filtering_demo.py
@@ -1,9 +1,7 @@
 import numpy as np
 
 array_1d = np.array([1,10,45,30,2,100])
-print(array_1d > 20) # [False False  True  True]
-print(~ (array_1d > 20)) # [ True  True False False]
-print(array_1d[array_1d > 20]) # [ 45 30 ]
-print(array_1d[ ~ (array_1d > 20)]) # [ 1 10]
-
-array_2d = np.array([[10,23,np.nan],100,np.nan,100],[34,56,78])
+print(array_1d > 20) # [False False  True  True False  True]
+print(~(array_1d > 20)) # [ True  True False False  True False]
+print(array_1d[array_1d > 20]) # [ 45  30 100]
+print(array_1d[~(array_1d > 20)]) # [ 1 10  2]
--- numpydemo/7-where_demo.py
+++ numpydemo/7-where_demo.py
@@ -4,6 +4,7 @@ array_1d = np.array([1,2,3,4,5,6,7,9,10])
 
 print("even array using where", array_1d[np.where(array_1d % 2 == 0)]) # this is similar to fancy indexing
 print("calculated array using where", np.where(array_1d % 2 == 0, array_1d*2, array_1d))
+print(np.where(array_1d % 2 == 0))
 
 ## fancy indexing
 indexes = [0,4,5]
