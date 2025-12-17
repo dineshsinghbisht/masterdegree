03e5385880455019305de7702d4fb8b651143b3b
dinesh <dinesh.bisht@gmail.com>
2025-12-17 14:36:49 +0200

    update main repo

--- panda_demo/100-Pandas_ReadMe.md
+++ panda_demo/100-Pandas_ReadMe.md
@@ -1,4 +1,20 @@
-tutorials
-
 https://www.youtube.com/watch?v=qrMnoY8qBJM
+
+
+Pandas
+*******
+
+print(df) df.head() df.tail() df.info() df.describe 
+
+df.shape df.columns
+
+index=False
+
+encoding="utf-8" "latin1"
+
+df["col"]
+df[df["col"] > 8]
+
+
+
 
--- panda_demo/5-select-cols-demo.py
+++ panda_demo/5-select-cols-demo.py
@@ -1,15 +1,25 @@
 import numpy as np
 import pandas as pd
 
+# iloc vs loc
 
 ## read csv
 df = pd.read_csv("sample_students.csv")
 print("Shape is", df.shape)
 print("Columns are:", df.columns)
-#print("Select Name Col only", df[["Name"]])
+print("Select Name Col only", df[["Name"]]) # Output would be Pandas DataFrame
+print("Select Name Col only", df["Name"]) # the output would be Pandas Series
 #print("Select Name & Age Col only", df[["Name", "Age"]])
 
 df["Student"] = "Yes"
 df['is_less_than_25'] = np.where(df['Age'] < 25, 'Yes', 'No')
 
 print(df)
+
+df_first_col = df.iloc[:, :1] # retruns Series
+print(df_first_col)
+df_rest_cols = df.iloc[:, 1:]
+print(df_rest_cols)
+
+df_last_col = df.iloc[:, -1:]
+df_except_last = df.iloc[:, :-1]
