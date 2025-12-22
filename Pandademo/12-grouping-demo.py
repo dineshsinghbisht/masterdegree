4383f20c08c90cc21bd91fcd55007d5063de0e77
dinesh <dinesh.bisht@gmail.com>
2025-12-22 17:00:14 +0200

    update main repo

--- Pandademo/12-grouping-demo.py
+++ Pandademo/12-grouping-demo.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 ## read csv
 df = pd.read_csv("sample_students.csv")
-print("Shape is", df.shape)
-print("Columns are:", df.columns)
-print(df.head())
+# print("Shape is", df.shape)
+# print("Columns are:", df.columns)
+# print(df.head())
 grouped = df.groupby("City")["Age"].sum()
-print(grouped)
+# print(grouped)
 
 
 df = pd.DataFrame({
@@ -16,9 +16,15 @@ df = pd.DataFrame({
     "EmployeeID": [123, 456, 467, 23423, 9999]
 })
 
-print(df.groupby("Department")["Salary"].mean())
-print(df.groupby("Department", as_index=False)["Salary"].mean())
-print(df.groupby("Department")["Salary"].agg(["mean", "min", "max"]))
+print((df["Department"].value_counts()))
+dep_counts = df["Department"].value_counts()
+print(dep_counts.info())
+print(dep_counts.index)
+print(dep_counts.values)
+
+# print(df.groupby("Department")["Salary"].mean())
+# print(df.groupby("Department", as_index=False)["Salary"].mean())
+# print(df.groupby("Department")["Salary"].agg(["mean", "min", "max"]))
 
 df.groupby("Department").agg({
     "Salary": ["mean", "sum"],
